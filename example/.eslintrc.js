b5cf36e14d3ad69d8d9eb5a37b2d05921bfabd23
Laurens Lamberts <laurens.lamberts@iodigital.com>
2024-01-23 20:53:04 +0100

    added eslintrc

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -2,6 +2,16 @@ module.exports = {
   root: true,
   extends: '@react-native',
   rules: {
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      {
+        enableDangerousAutofixThisMayCauseInfiniteLoops: true,
+        additionalHooks:
+          '(useAnimatedStyle|useAnimatedProps|useDerivedValue|useAnimatedGestureHandler)',
+      },
+    ],
+    '@typescript-eslint/prefer-optional-chain': 'warn',
+    'object-shorthand': ['warn', 'always'],
     'react-native/no-inline-styles': 'off',
   },
 };
