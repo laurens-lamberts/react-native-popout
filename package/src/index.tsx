63a5988ddc1d1fdfc16d5e8aedc32d6dd3707217
Laurens Lamberts <laurens.lamberts@iodigital.com>
2024-04-27 22:47:22 +0200

    removed type re-export from index as it goes missing when turning to JS

--- package/src/index.tsx
+++ package/src/index.tsx
@@ -1,7 +1,6 @@
 import PopoutRootView, { PopoutContext } from './components/PopoutRootView';
 import PopoutTile from './components/PopoutTile';
 import PopoutOverlayContent from './screens/PopoutOverlayContent';
-import { PopoutTileType } from './types/PopoutTile';
 
-export { PopoutTile, PopoutContext, PopoutTileType, PopoutOverlayContent };
+export { PopoutTile, PopoutContext, PopoutOverlayContent };
 export default PopoutRootView;
